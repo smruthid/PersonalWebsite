326b1d0c6dd8de20870323c3d59f7fc15b364663
smruthid <danda.smruthi@gmail.com>
2023-12-21 08:56:24 -0800

    modularizing the app by adding components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TypewriterComponent } from './typewriter/typewriter.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TypewriterComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/typewriter/typewriter.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-typewriter',
+  templateUrl: './typewriter.component.html',
+  styleUrls: ['./typewriter.component.css']
+})
+export class TypewriterComponent {
+
+}
