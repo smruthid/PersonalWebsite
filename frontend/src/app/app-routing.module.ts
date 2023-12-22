bc6e4e0ca6484f98c59a5da4d5cdd10f2689f7a9
smruthid <danda.smruthi@gmail.com>
2023-12-22 09:52:33 -0800

    modularizing components and adding components and paths for different routes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutmeComponent } from './aboutme/aboutme.component';
+import { TravelComponent } from './travel/travel.component';
+import { ExperienceComponent } from './experience/experience.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'aboutme',
+    component: AboutmeComponent
+  },
+  {
+    path: 'travel',
+    component: TravelComponent
+  },
+  {
+    path: 'experience',
+    component: ExperienceComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,12 +7,20 @@ import { TypewriterComponent } from './typewriter/typewriter.component';
 import { NavComponent } from './nav/nav.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
+import { HomeComponent } from './home/home.component';
+import { AboutmeComponent } from './aboutme/aboutme.component';
+import { TravelComponent } from './travel/travel.component';
+import { ExperienceComponent } from './experience/experience.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TypewriterComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent,
+    AboutmeComponent,
+    TravelComponent,
+    ExperienceComponent
   ],
   imports: [
     BrowserModule,
