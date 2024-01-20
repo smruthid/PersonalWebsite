40a893de745dfa3b76531234b827dc56c788b1a0
smruthid <danda.smruthi@gmail.com>
2024-01-19 21:12:01 -0800

    adding experience

--- frontend/src/app/experience/experience.component.ts
+++ frontend/src/app/experience/experience.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./experience.component.css']
 })
 export class ExperienceComponent {
-
+  myExperience: any[] = [
+    {
+      company: "Meta",
+      startDate: "September 2022",
+      endDate: "Present",
+      tasks: [ 
+        "Firmware implementation of VR Prototyping Platform", 
+        "Using Visual Basic C# with VSCode to implement GUI so users can use internal firmware with Prototyping Platform"],
+      skills: [
+        "C", "C++", "Arduino", "Bash", "FreeRTOS", "STM Microprocessors", "Logic Analyzers"
+      ]
+    },
+    {
+      company: "Microchip",
+      startDate: "April 2021",
+      endDate: "September 2022",
+      tasks: [ 
+        "Worked on Libero programming tools to generate bitstream for PolarFire FPGAs using C++", 
+        "Updated firmware to update FlashPro6 programmers to latest design using the USB protocol"
+      ],
+      skills: [
+        "C++", "Python", "FPGAs"
+      ]
+    }
+  ]
 }
