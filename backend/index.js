fba54acb752109e8ececb503d556be379bce4bca
smruthid <danda.smruthi@gmail.com>
2024-01-24 19:15:21 -0800

    changing for heroku

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const dotenv = require('dotenv').config();
 const cors = require('cors');
 const app = express();
+const port = process.env.PORT || 8080;
 
 app.use(express.json())
 app.use(cors())
