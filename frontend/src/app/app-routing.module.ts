09b127fcb26f50d581f52bc6f887cf12c7eee66c
smruthid <danda.smruthi@gmail.com>
2024-02-25 16:07:29 -0800

    changing format of experience page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,10 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutmeComponent } from './aboutme/aboutme.component';
-import { TravelComponent } from './travel/travel.component';
 import { ExperienceComponent } from './experience/experience.component';
 import { ContactComponent } from './contact/contact.component';
-import { BooksComponent } from './books/books.component';
 
 const routes: Routes = [
   {
@@ -17,20 +15,12 @@ const routes: Routes = [
     component: AboutmeComponent
   },
   {
-    path: 'travel',
-    component: TravelComponent
-  },
-  {
     path: 'experience',
     component: ExperienceComponent
   },
   {
     path: 'contact',
     component: ContactComponent
-  },
-  {
-    path: 'books',
-    component: BooksComponent
   }
 ];
 
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -9,7 +9,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
 import { HomeComponent } from './home/home.component';
 import { AboutmeComponent } from './aboutme/aboutme.component';
-import { TravelComponent } from './travel/travel.component';
 import { ExperienceComponent } from './experience/experience.component';
 import { ContactComponent } from './contact/contact.component';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -17,11 +16,8 @@ import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
-import {MatListModule} from '@angular/material/list';
-
-import { BooksComponent } from './books/books.component';
-import { CarouselComponent } from './carousel/carousel.component';
-
+import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 
 @NgModule({
@@ -31,11 +27,8 @@ import { CarouselComponent } from './carousel/carousel.component';
     NavComponent,
     HomeComponent,
     AboutmeComponent,
-    TravelComponent,
     ExperienceComponent,
     ContactComponent,
-    BooksComponent,
-    CarouselComponent
   ],
   imports: [
     BrowserModule,
@@ -48,6 +41,7 @@ import { CarouselComponent } from './carousel/carousel.component';
     MatIconModule,
     MatDividerModule,
     MatListModule,
+    MatGridListModule,
     HttpClientModule
   ],
   providers: [],
